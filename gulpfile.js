561bf4dcf228b34f028296a91b6656c893b8284c
Adracus <adracus@gmail.com>
2015-03-03 13:51:00 +0100

    Add gulp run-sequential and pdf task

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,22 @@
 var gulp		= require("gulp");
 var pandoc 		= require("gulp-pandoc");
+var runSequence = require("run-sequence");
 
 
 var paths = {
 	md_folder 	: "md/",
 	tex_folder 	: "tex/",
 	html_folder : "html/",
+	pdf_folder	: "pdf/",
 	md_files 	: "md/**/*.md",
 	tex_files	: "tex/**/*.tex",
-	html_files 	: "html/**/*.html"
+	html_files 	: "html/**/*.html",
+	pdf_files 	: "pdf/**/*.pdf"
 };
 
 
 gulp.task("tex", function() {
-	gulp.src(paths.md_files)
+	return gulp.src(paths.md_files)
 		.pipe(pandoc({
 			from: 	"markdown_github",
 			to: 	"latex",
@@ -24,14 +27,26 @@ gulp.task("tex", function() {
 });
 
 gulp.task("html", function() {
-	gulp.src(paths.md_files)
+	return gulp.src(paths.md_files)
     	.pipe(pandoc({
-			from: 'markdown',
-			to: 'html5',
-			ext: '.html',
-			args: ['--smart']
+			from: 	'markdown',
+			to: 	'html5',
+			ext: 	'.html',
+			args: 	['--smart']
     	}))
     	.pipe(gulp.dest(paths.html_folder));
 });
 
-gulp.task("default", ["tex"]);
+gulp.task("pdf", function() {
+	return gulp.src(paths.tex_files)
+		.pipe(pandoc({
+			from: 	"latex",
+			to: 	"pdf",
+			ext: 	".pdf",
+			args: 	["--smart"]
+		}))
+});
+
+gulp.task("default", function(cb) {
+	runSequence("tex", "pdf", cb);
+});
